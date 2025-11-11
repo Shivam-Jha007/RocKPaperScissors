49089c2a6680d88a2ac06c6f9068ae5c607c8f7f
Shivam Jha <just4work442@gmail.com>
2025-11-12 01:11:22 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+let userScore=0;
+let compScore=0;
+const choices=document.querySelectorAll(".choice");
+choice.foreach(choice)=>{
+    console.log(choices);
+    choice.addEventlistener("click", ()=>{
+        console.log("choice was clicked");
+    })
+
+}
